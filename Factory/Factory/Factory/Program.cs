9667d2e4ef85d2abc6b38fb7cad675e228356c0d
Tharumal <tharumalwimalarathna@gmail.com>
2022-09-01 20:36:48 +0530

    test

---
+++ Factory/Factory/Factory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+   
+public class Foo
+    {
+        public Foo()
+        {
+        }
+        public  async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
